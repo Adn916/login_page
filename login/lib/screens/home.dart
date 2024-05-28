dc55b956e3636c64033679de7a8aec3c94735767
Gokul-Gokzzz <gokulunniu88@gmail.com>
2024-05-28 10:20:21 +0530

    changed

--- login/lib/screens/home.dart
+++ login/lib/screens/home.dart
@@ -8,33 +8,34 @@ class ScreenHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Align(alignment: Alignment.center, child: Text("HOME")),
-      backgroundColor: Colors.green,
-      leading: Icon(Icons.home),
-      actions: [
-        IconButton(onPressed: (){
-          signout(context); 
-        }, icon: Icon(Icons.exit_to_app),)
-      ],
+      appBar: AppBar(
+        title: Align(alignment: Alignment.center, child: Text("HOME")),
+        backgroundColor: Colors.green,
+        leading: Icon(Icons.home),
+        actions: [
+          //   IconButton(onPressed: (){
+          //     signout(context);
+          //   }, icon: Icon(Icons.exit_to_app),)
+        ],
       ),
       body: ListView.builder(
         itemCount: 100,
         itemBuilder: (context, index) => Card(
-        child: ListTile(
-          trailing: Icon(Icons.nat),
-          leading: Icon(Icons.abc),
-          title: Text("hello $index"),
+          child: ListTile(
+            trailing: Icon(Icons.nat),
+            leading: Icon(Icons.abc),
+            title: Text("hello $index"),
+          ),
         ),
-      ),),
+      ),
     );
-  } 
- 
-  signout(BuildContext ctx) async{
-    final _sharedpre=await SharedPreferences.getInstance();
+  }
+
+  signout(BuildContext ctx) async {
+    final _sharedpre = await SharedPreferences.getInstance();
     await _sharedpre.clear();
 
     Navigator.of(ctx).pushAndRemoveUntil(
         MaterialPageRoute(builder: (ctx) => ScreenLogin()), (route) => false);
-
   }
 }
